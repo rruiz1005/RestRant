024124ba9f2566d0b93e4c4ea7d447b24fb11342
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-05-04 19:59:07 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -27,7 +27,14 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    res.send(`This is GET /places/${req.params.id}`)
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+        res.render('error404')
+    } else {
+        res.render('places/show', {place: places[id]})
+    }
 })
 
 router.get('/:id/edit', (req, res) => {
