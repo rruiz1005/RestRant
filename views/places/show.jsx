dccdf0cf699afd401d9ba24b7a4d0d7410f87d37
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-05-20 12:10:41 -0400

    create and use helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,25 +5,43 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
-                <img src='/images/coffeeshop.jpg'></img>
-                <div>
-                    <h3>Rating</h3>
-                    <p>Not Rated</p>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <img src={data.place.pic} alt={data.place.name}></img>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className='col-sm-6'>
+                        <h1>{data.place.name}</h1>
+                        <div>
+                            <h3>Rating</h3>
+                            <p>Not Rated</p>
+                        </div>
+                        <h2>
+                            Description
+                        </h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                        <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                            Edit
+                        </a>
+                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                            <button type='submit' className='btn btn-danger'>
+                                Delete
+                            </button>
+                        </form>
+                    </div>
+                        <hr></hr>
+                        <div>
+                            <h3>Comments</h3>
+                            <p>No comments yet!</p>
+                        </div>
                 </div>
-                <hr></hr>
-                <div>
-                    <h3>Comments</h3>
-                    <p>No comments yet!</p>
-                </div>
-                <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
-                    Edit
-                </a>
-                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-                    <button type='submit' className='btn btn-danger'>
-                        Delete
-                    </button>
-                </form>
             </main>
         </Def>
     )
