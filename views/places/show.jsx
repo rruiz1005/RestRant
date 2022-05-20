2109b573175b178f9cf691eae1a054053c5fa715
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-05-20 13:32:19 -0400

    implement the comment form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -11,7 +11,7 @@ function show (data) {
         comments = data.place.comments.map(c => {
             return (
             <div className="border">
-                <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
                 <h4>{c.content}</h4>
                 <h3>
                     <stong>- {c.author}</stong>
@@ -55,11 +55,39 @@ function show (data) {
                             </button>
                         </form>
                     </div>
-                        <hr></hr>
-                        <div>
-                            <h3>Comments</h3>
-                            {comments}
-                        </div>
+                    <hr></hr>
+                    <div>
+                        <h3>Comments</h3>
+                        {comments}
+                    </div>
+
+                    <hr></hr>
+                    <h2>Got Your Own Rant or Rave?</h2>
+        <form method='POST' action={`/places/${data.place.id}/comment`}>
+            <div className="row">
+                <div className="form-group col-sm-12">
+                    <label htmlFor="content">Say Whats On Your Mind!</label>
+                    <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+            </div>
+            <div className="row">
+                <div className="form-group col-sm-4">
+                    <label for="author">Author</label>
+                    <input id="author" name="author" className="form-control" />
+                </div>
+                <div id="slidecontainer" className="form-group col-sm-4">
+                    <label htmlFor="stars">Star Rating</label>
+                    <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                    <div className='row'>
+                        <label htmlFor="rant">Rant?</label>
+                        <input type="checkbox" id="rant" name="rant" />
+                    </div>
+                </div>
+            </div>
+            <input type="submit" className="btn btn-primary" value="Add Comment" />
+        </form>
                 </div>
             </main>
         </Def>
