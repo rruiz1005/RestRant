c9fdb178e35f4dea2cc34c8297fc4eb7d826f466
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-05-20 13:48:47 -0400

    posting comments functionality

--- controllers/places.js
+++ controllers/places.js
@@ -62,10 +62,31 @@ router.post('/', (req, res) => {
     })
 })
 
-
-router.post('/:id/rant', (req, res) => {
-    res.send('POST /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    if (req.body.author === '') {
+        req.body.author = undefined
+    }
+    req.body.rant = req.body.rant ? true : false
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`);
+            })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
 })
+
 
 //PUT
 router.put('/:id', (req, res) => {
